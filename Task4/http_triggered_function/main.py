9540478a08cc91c78e7fb6b98811aa813956effd
Vadym <popikvi@gmail.com>
2023-05-20 23:03:25 +0300

    Task4 init

---
+++ Task4/http_triggered_function/main.py
@@ -0,0 +1,48 @@
+import uuid
+import json
+import os
+import time
+from google.cloud import pubsub_v1
+from flask import Request
+
+publisher = pubsub_v1.PublisherClient()
+PROJECT_ID = os.environ.get('PROJECT_ID')
+TOPIC_NAME = os.environ.get('TOPIC_NAME')
+
+
+def http_triggered_function(request: Request):
+
+    data = request.data
+
+    if data is None:
+        print('Request is empty')
+        return 'Request is empty', 400
+
+    print(f"Request data: {data}")
+
+    data_json = json.loads(data)
+    event_name = data_json['event_name']
+
+    request_id = str(uuid.uuid4())
+    timestamp = int(time.time())
+
+    topic_path = f'projects/{PROJECT_ID}/topics/{TOPIC_NAME}'
+
+    message_json = json.dumps({
+        "data": {
+            "event_name": event_name,
+            "request_id": request_id,
+            "processed_at": timestamp
+        }
+    })
+
+    message_bytes = message_json.encode('utf-8')
+
+    try:
+        publish_future = publisher.publish(topic_path, data=message_bytes)
+        publish_future.result()
+    except Exception as e:
+        print(e)
+        return e, 500
+
+    return 'Message received and published to Pub/Sub', 200
---
+++ Task4/variables.tf
@@ -0,0 +1,15 @@
+variable "credentials_path" {
+  default = "/home/vadymor/PycharmProjects/centered-motif-229719-eca2d2c6ea45.json"
+}
+
+variable "project_id" {
+  default = "centered-motif-229719"
+}
+
+variable "bucket_name" {
+  default = "tf_events_storage"
+}
+
+variable "stream_topic_name" {
+  default = "tf_event_stream"
+}
